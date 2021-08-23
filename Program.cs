311f7ff54414d05695724a7a66e0be15dd5a7add
Steve Fagerhaug <sven@isnowhere.org>
2021-08-23 18:07:16 -0400

    add azure queue message relay

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Statiq.Common;
 using Statiq.App;
 using Statiq.Web;
 
@@ -10,7 +13,27 @@ namespace MySite
       await Bootstrapper
         .Factory
         .CreateWeb(args)
-        .RunAsync();
-  }
+        .AddSetting(Keys.Host, new Uri(Constants.SiteUri).Host)
+        .AddSetting(Keys.LinksUseHttps, true)
+        .AddSetting(WebKeys.ExcludedPaths,
+            	new List<NormalizedPath>
+            	{
+                	new NormalizedPath("input/admin"),
+            	})
+        // .AddSetting(
+        //   Keys.DestinationPath,
+        //   Config.FromDocument((doc, ctx) =>
+        //   {
+        //       // Only applies to the content pipeline
+        //       if (ctx.PipelineName == nameof(Statiq.Web.Pipelines.Content))
+        //       {
+        //           return doc.Source.Parent.Segments.Last().SequenceEqual("posts".AsMemory())
+        //               ? new NormalizedPath(Constants.BlogPath).Combine(doc.GetDateTime(WebKeys.Published).ToString("yyyy")).Combine(doc.GetString("Category")).Combine(doc.Destination.FileName.ChangeExtension(".html"))
+        //               : doc.Destination.ChangeExtension(".html");
+        //       }
+        //       return doc.Destination;
+        //   }))        
+      .RunAsync();
+}
 }
 
